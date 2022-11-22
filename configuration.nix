664fa003f3fc957b807c50a0d1f5308a773163bb
David Rose <doppler@gmail.com>
2022-11-22 14:53:33 -0500

    initial commit

---
+++ configuration.nix
@@ -0,0 +1,183 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.utf8";
+
+  # Bypass sudo password for users in wheel group
+  security.sudo.wheelNeedsPassword = false;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  services.gnome.gnome-browser-connector.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "us";
+    xkbVariant = "dvorak";
+    xkbOptions = "ctrl:nocaps";
+  };
+
+  # Configure console keymap
+  console.keyMap = "dvorak";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Enable libvirtd and install virt-manager
+  virtualisation.libvirtd.enable = true;
+  programs.dconf.enable = true;
+
+  # Enable zsh with OhMyZsh
+  programs.zsh.enable = true;
+
+  programs.zsh.ohMyZsh = {
+    enable = true;
+    plugins = [ "git" ];
+    theme = "agnoster";
+  };
+
+  # programs.sway = {
+  #   enable = true;
+  #   extraPackages = with pkgs; [ i3status-rust rofi ];
+  # };
+
+  # Enable RTL-SDR
+  hardware.rtl-sdr.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.doppler = {
+    isNormalUser = true;
+    description = "doppler";
+    extraGroups = [ "networkmanager" "wheel" "libvirtd" "plugdev" ];
+    shell = pkgs.zsh;
+    packages = with pkgs; [
+      alacritty
+      bitwarden
+      btop
+      firefox
+      gimp
+      google-chrome
+      gqrx
+      imagemagick
+      inkscape
+      slack
+      vscode
+    ];
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    deno
+    ffmpeg
+    gcc
+    git
+    gnumake
+    imv
+    mpv
+    nodejs
+    pkgs.gnome.dconf-editor
+    pkgs.gnome3.gnome-tweaks
+    pkgs.heroku
+    pkgs.sass
+    python3
+    unzip
+    vim
+    virt-manager
+    yarn
+  ];
+
+  # try to use a hosts file to block ads and shit.
+  # https://github.com/StevenBlack/hosts#nixos
+  # networking.extraHosts = let
+  #   hostsPath = https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts;
+  #   hostsFile = builtins.fetchurl hostsPath;
+  #   sha256 = "00000000000000000000000000000000000000000000000000000000";
+  # in builtins.readFile "${hostsFile}";
+
+  # fonts.fonts = with pkgs; [
+  #   noto-fonts
+  #   noto-fonts-emoji
+  #   fira-code
+  #   fira-code-symbols
+  # ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.05"; # Did you read the comment?
+
+}
