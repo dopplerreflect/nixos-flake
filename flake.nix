9f61da4c47851c715af4756912a6b4ce955d93b2
David Rose <doppler@gmail.com>
2023-01-10 07:53:58 -0700

    remove StephenBlackHosts

--- flake.nix
+++ flake.nix
@@ -5,10 +5,15 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    hosts.url = "github:StevenBlack/hosts";
+    # hosts.url = "github:StevenBlack/hosts";
   };
 
-  outputs = inputs@{ nixpkgs, hosts, home-manager, ... }: 
+  outputs = inputs@{ 
+    nixpkgs, 
+    # hosts, 
+    home-manager,
+     ... 
+     }: 
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -22,9 +27,9 @@
           inherit system;
           modules = [
             ./configuration.nix
-            hosts.nixosModule {
-              networking.stevenBlackHosts.enable = true;
-            }
+            # hosts.nixosModule {
+            #   networking.stevenBlackHosts.enable = true;
+            # }
             home-manager.nixosModules.home-manager {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
